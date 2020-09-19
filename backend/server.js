28832c3c5feb08893d2045576eaf1b95424e5589
Ananya Teklu <teklu009@morris.umn.edu>
2020-09-19 18:50:37 -0500

    Server now connects to the database

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,18 @@ require('dotenv').config();
 const port = process.env.PORT || 4000;
 const app = express();
 
+
 app.use(express.json());
 app.use(cors());
+
+//Create a connection with the database
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB successfully connected");
+})
 
 //server listens to the given port
 app.listen(port, function(error) {
