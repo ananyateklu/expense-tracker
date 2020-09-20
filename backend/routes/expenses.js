bbb465c98fb64b195d47c6d33c255efa2f843c68
Ananya Teklu <teklu009@morris.umn.edu>
2020-09-19 20:33:52 -0500

    Create a route that handles getting all expenses from the DB and adding new expense the DB

---
+++ backend/routes/expenses.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const Expense = require('../models/expense.model');
+
+//This route gets files from the database
+router.route('/').get((req, res) => {
+    Expense.find()
+        .then(expenses => res.json(expenses))
+        .catch(err => res.status(400).json('Error:' + err));
+});
+
+//This route adds files to the database 
+router.route('/add').post((req,res) => {
+    const expensetype = req.body.expensetype;
+    const itemname = req.body.itemname;
+    const amount = req.body.amount;
+    const description = Number(req.body.description);
+    const date = Date.parse(req.body.date);
+
+    const newExpense = new Expense({
+        expensetype: expensetype,
+        itemname: itemname,
+        amount: amount,
+        description,
+        date
+});
+
+newExpense.save()
+.then(() => res.json('Expense added!'))
+.catch(err => res.status(400).json('Error:' + err));
+});
+
+
+
+
+module.exports = router;
