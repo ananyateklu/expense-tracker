c09e0439bf90548d093ae23c34f4d908adccda56
Ananya Teklu <teklu009@morris.umn.edu>
2020-09-19 21:40:54 -0500

    create simple react nav bar

---
+++ frontend/src/components/navbar.component.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class Navbar extends Component {
+
+    render() {
+        return (
+            <nav className="navbar navbar-light bg-light navbar-expand-md">
+                <Link to="/" className="navbar-brand">ExpenseTracker</Link>
+                <div className="collapse navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="navbar-item">
+                    <Link to="/" className="nav-link">Expenses</Link>
+                    </li>
+                    <li className="navbar-item">
+                    <Link to="/create" className="nav-link"> Add New Expense</Link>
+                    </li>
+                    <li className="navbar-item">
+                    <Link to="/user" className="nav-link">Create User</Link>
+                    </li>
+                </ul>
+                    </div> 
+            </nav>
+
+        );
+    }
+
+}
