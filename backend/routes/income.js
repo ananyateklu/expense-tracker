20d5d33cff84bd596b4b6a607e064b90055f535d
Ananya Teklu <teklu009@morris.umn.edu>
2020-09-19 20:40:31 -0500

    Create a route that handles getting all income from the DB and adding new income to the DB

---
+++ backend/routes/income.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const Income = require('../models/income.model');
+
+//This route gets files from the database
+router.route('/').get((req, res) => {
+    Income.find()
+        .then(income => res.json(income))
+        .catch(err => res.status(400).json('Error:' + err));
+});
+
+//This route adds files to the database 
+router.route('/add').post((req,res) => {
+    const incometype = req.body.incometype;
+    const amount = req.body.amount;
+    const date = Date.parse(req.body.date);
+
+    const newIncome = new Income({
+        incometype: incometype,
+        amount: amount,
+        date
+});
+
+newIncome.save()
+.then(() => res.json('Income added!'))
+.catch(err => res.status(400).json('Error:' + err));
+});
+
+
+
+
+module.exports = router;
