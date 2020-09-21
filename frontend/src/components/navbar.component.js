0fbc77e685a6a5e90e22ccc6137805e5fa781385
Ananya Teklu <teklu009@morris.umn.edu>
2020-09-20 21:20:24 -0500

    New navbar with better features

--- frontend/src/components/navbar.component.js
+++ frontend/src/components/navbar.component.js
@@ -1,27 +1,29 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-export default class Navbar extends Component {
-
+export default class NavBar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-light navbar-expand-sm" style={{ backgroundColor: "#3d70b2" }}>
-                <Link to="/" className="navbar-brand">ExpenseTracker</Link>
-                <div className="collapse navbar-collapse">
-                <ul className="navbar-nav mr-auto">
-                    <li className="navbar-item">
-                    <Link to="/" className="nav-link">Expenses</Link>
-                    </li>
-                    <li className="navbar-item">
-                    <Link to="/create" className="nav-link"> Add New Expense</Link>
-                    </li>
-                    <li className="navbar-item">
-                    <Link to="/user" className="nav-link">Create User</Link>
-                    </li>
-                </ul>
-                    </div> 
-            </nav>
+            <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+            <Navbar.Brand href="/">Expense-Tracker</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/">Expenses</Nav.Link>
+                <Nav.Link href="/income">income</Nav.Link>
+                <NavDropdown title="Add" id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="/create">expense</NavDropdown.Item>
+                  <NavDropdown.Item href="/createincome">income</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Nav>
+                <Nav.Link href="/user">Username</Nav.Link>
 
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
         );
     }
 
