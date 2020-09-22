1db54c91a0f30b4a6fe9523003f7a3f780f9b530
Ananya Teklu <teklu009@morris.umn.edu>
2020-09-22 16:04:40 -0500

    Reimplimenting some methods for clarity and ease of use

---
+++ frontend/src/components/entry.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Entry() {
+
+    return (
+        <div className="list-group">
+        <div className="list-group-item list-group-item-action flex-column align-items-start">
+          <div className="d-flex w-100 justify-content-between">
+          <div className="mb-1">U</div>
+            <h5 className="mb-1">Rent</h5>
+            <small>$65.99</small>
+          </div>
+             <small >3 days ago</small>
+       
+         
+         
+        </div>
+      </div>
+    );
+}
